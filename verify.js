fac868dd2860935abe2bed6815ca489c3456e894
Adesina Surprise <adesinasurprise2006@gmail.com>
2024-04-08 20:22:02 +0100

    My portfolio

---
+++ verify.js
@@ -0,0 +1,9 @@
+// Function to display message with animation
+function showMessage(message) {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = message;
+    messageElement.style.display = 'block';
+  }
+  // Example usage:
+  showMessage('Thanks for reaching out to Tobi David. We would get back to you as soon as possible.');
+  
